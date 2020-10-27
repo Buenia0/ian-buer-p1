025e93efa27d45a2ad49fbee67494bd81f202335
Buenia0 <buenia.mbemu@gmail.com>
2020-10-27 17:09:21 -0500

    Completed Project 1.

---
+++ js/master.js
@@ -0,0 +1,11 @@
+window.onload = function()
+{
+    var text1 = document.getElementById("text1");
+    text1.innerHTML = "A first for Game Boy emulators";
+
+    var text2 = document.getElementById("text2");
+    text2.innerHTML = "An upgrade for epic netplay";
+
+    var text3 = document.getElementById("text3");
+    text3.innerHTML = "And more to come";
+}
